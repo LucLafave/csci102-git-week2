d24d3415fb0fff330aade27bb104b1a9c17e451f
LucLafave <luclafave@mymail.mines.edu>
2019-11-21 10:25:05 -0700

    fixed fibonnaci

---
+++ fib.py
@@ -0,0 +1,19 @@
+# fibonacci.py
+
+def fib():
+    fibs = [1, 2]
+    x = 0
+    y = 0
+    num = 1
+    for i in range(1,99):
+        if i == 0:
+            continue
+        else:
+            fibs.append(fibs[i-1] + fibs[i])
+    return fibs
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
+    
